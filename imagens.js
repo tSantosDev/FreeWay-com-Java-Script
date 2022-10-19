dc67c31bef4b9a641b93950c2a24400a39db483d
Thiago Santos Ferreira <thiago.estudo1603@gmail.com>
2022-10-19 12:11:38 -0300

    Update imagens.js

---
+++ imagens.js
@@ -0,0 +1,28 @@
+//Imagens e Sons do Jogo
+
+//Variáveis das Imagens
+let imagemEstrada;
+let imagemAtor;
+let imagemCarro1;
+let imagemCarro2;
+let imagemCarro3;
+let imagemAtor2;
+
+//Variáveis dos Sons
+let trilhaSonora;
+let somColisao;
+let somPonto;
+
+function preload(){
+  imagemEstrada = loadImage("imagens/estrada.png");
+  imagemAtor = loadImage("imagens/ator-1.png");
+  imagemAtor2 = loadImage("imagens/ator-2.png");
+  imagemCarro1 = loadImage("imagens/carro-1.png");
+  imagemCarro2 = loadImage("imagens/carro-2.png");
+  imagemCarro3 = loadImage("imagens/carro-3.png");
+  imagemCarros = [imagemCarro1, imagemCarro2, imagemCarro3, imagemCarro1, imagemCarro2, imagemCarro3];
+  trilhaSonora = loadSound("sons/trilha.mp3");
+  somColisao = loadSound("sons/colidiu.mp3");
+  somPonto = loadSound("sons/pontos.wav");
+
+}
